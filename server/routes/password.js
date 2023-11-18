5c88ba4a48d967481b1d41b9d9cd490bc2abaf29
TechUX <connect.world12345@gmail.com>
2023-11-18 22:35:50 +0530

    Added password route

---
+++ server/routes/password.js
@@ -0,0 +1,13 @@
+const express = require("express") ;
+const dbConnect = require("../component/dbconfig") ;
+const dotenv = require("dotenv") ;
+
+dotenv.config()
+
+var router = express.Router();
+
+
+
+
+
+module.exports = router ;
